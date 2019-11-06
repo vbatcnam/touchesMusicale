823a0cea93e09945142f584106bc72192ad22fcb
Vero <vero@famille.fr>
2019-11-06 23:07:57 +0100

    avancé dans code fini attributUnHertz

--- script/cubes/NoteDeMusique.js
+++ script/cubes/NoteDeMusique.js
@@ -5,39 +5,39 @@
 	Copyright : © Véronique Lion 2019
 */
 
+var gamme = ['do', 'do#', 'ré', 'ré#', 'mi', 'fa', 'fa#', 'sol', 'sol#', 'la','la#','si'];
+
 class NoteDeMusique extends SCCube{
 	constructor(nom, octave, x, y){
 		super(); //Cube
 		this.nom = nom;
+		this.noteNum = attributUnNumero();
 		this.octave = octave;
 		this.x = x; 
 		this.y = y;
-		this.color = this.getColor();
-		this.hertz = this.getHertz();
+		this.color = this.attributUneCouleurHSL();
+		this.hertz = this.attributUnHertz();
 		this.illustration = this.DessineMoi();
 	}
 	
-	static getColor()
+	static attributUnNumero(){
+		let num = gamme.indexOf(this.nom);
+		if(this.octave == 0){
+			return num;
+		}else{
+			return 12*octave + num;
+		}
+	}
+	
+	static attributUneCouleur()
 	{
 		/**
-			couleurs de l'arc en ciel
-				Do rouge : 0
-				Ré orange: 30
-				Mi jaune : 60
-				Fa vert : 120
-				Sol aqua: 180
-				La bleu: 240
-				Si violet: 270
-			Pour les # on change la saturation
-				note normale saturation 100%, note # saturation 50%
+			Chaque note à une couleur de l'arc en ciel
 		*/
-		let tabColor = {'do':0, 'ré':30, 'mi':60, 'fa':120, 'sol':180, 'la':240, 'si':270};
-		let h = tabColor[this.nom]; //teinte
+		//teinte en %
+		let tabColor = [0, 15, 30, 45, 60, 120, 150, 180, 210, 240, 255,270];
+		let h = tabColor[this.noteNum]; //teinte
 		let s = 100; //saturation
-		//Pour les # on change la saturation
-		if(/#/.test(this.nom)){
-			s = s/2;
-		}
 		
 		/**
 			On se base sur le LA 440 hertz, octave 3 : la luminosité est à 50% 
@@ -48,26 +48,49 @@ class NoteDeMusique extends SCCube{
 		return 'hsl(${h},${s}%,${l}%)';
 	}
 	
-	static getHertz()
+	static attributUnHertz()
 	{
-		//On se base sur le LA de la 3e octave, soit 440 hertz. 
+		//On se base sur le LA (indice 9 du tableau gamme) de la 3e octave (noteNum 45), soit 440 hertz. 
 		/**
 		Une octave est constituée de 12 demi-tons. (les 6 notes + les notes#) 
 		Le double d'une fréquence donne un intervalle d'une octave.
-		pour calculer le rapport de fréquence entre 2 notes séparées d'un demi-tons il faut faire par exemple 440 * mdf et j’aurai le le la# de l'octave 3
-		Si je fais 440 / mdf, j’aurai sol# de l'octave 3
+		Pour calculer le rapport de fréquence entre 2 notes séparées d'un demi-tons il faut faire par exemple :
+			440 * mdf et j’aurai le le la# de l'octave 3
+			Si je fais 440 / mdf, j’aurai sol# de l'octave 3
 		
-		Si je fais 440 * mdf * mdf, j'aurai Si de l'octave 3
-		Si je fais 440 / mdf / mdf, j'aurai sol de l'octave 3
+			Si je fais 440 * mdf * mdf, j'aurai Si de l'octave 3
+			Si je fais 440 / mdf / mdf, j'aurai sol de l'octave 3
 		*/
 		let mdf =  Math.pow(2,1/12); //multiplicateurDeFrequence
-	}
-	
-	dessineMoi()
-	{
 		
+		/** nbreDemiTon
+			============
+			Nombre de demi tons qui sépare la note du LA octave 3 (noteNum 45)
+				Si positif la note se trouve après le LA 
+					on multiplie à la puissance de nbreDemiTon
+				sinon elle se trouve avant le LA
+					on divise par la puissance de nbreDemiTon
+		*/
+		let nbreDemiTon = noteNum - 45; // noteNum du LA octave 3
+		return 440 * Math.pow(mdf,nbreDemiTon);
 	}
 	
+
+	static dessineMoi(){
+		let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+		rect.id = this.nom;
+		// rect.setAttribute("stroke", this.color);
+		rect.setAttribute("fill", this.color);
+		rect.setAttribute("width", 50);
+		rect.setAttribute("height", 50);
+
+		rect.setAttribute("x", x);
+		rect.setAttribute("y", y);
+		let texte = document.createTextNode(id);
+		rect.appendChild(texte);
+		return rect;
+	}
+
 	//s'allume lorsqu'on clique ou touche la note
 	anime();
 	{
