ca1357bb748eadac799ef8a1e7aa72ee85fda827
Vero <vero@example.com>
2019-11-19 21:21:23 +0100

    avancé

--- script/miseEnScene.js
+++ script/miseEnScene.js
@@ -1,14 +1,18 @@
 'use strict';
 /**
 	Auteur : Véronique Lion
-	Date création : 2019-11-20
+	Date création : 2019-11-20 17h00
 	Copyright : © Véronique Lion 2019
 */
 
 
+//On récupère le zone SVG
+var zoneSVG = document.getElementById('zoneMusique');
+
+// création du contexte audio
 var contexteAudio = new AudioContext()
 
-//Dans ce programme il n'y a qu'un seuk instrument.
+//Dans ce programme il n'y a qu'un seul instrument.
 var wave_trombone = contexteAudio.createPeriodicWave(
 	new Float32Array(trombone.real), 
 	new Float32Array(trombone.imag)
@@ -17,5 +21,5 @@ var wave_trombone = contexteAudio.createPeriodicWave(
 instrument.addNote = monde.addProgram;
 
 //créer un tableau avec les notes (il y a 9 games, on va en faire 3.
-
-instrument.addNote(new NoteDeMusique(contexteAudio, nom, octave, x, y)));
+//teste une note
+instrument.addNote(new NoteDeMusique("la", 3, 10, 10)));
